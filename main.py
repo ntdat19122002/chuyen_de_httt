2e67549f725c2f1d1200a8c22c46b855586c2fdc
ntdat <ntdat19122002@gmail.com>
2024-05-18 00:18:13 +0700

    ok

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Khởi tạo mô hình cây quyết định
-clf = DecisionTreeClassifier(random_state=42)
+clf = DecisionTreeClassifier(max_depth=3,random_state=42)
 
 # Huấn luyện mô hình
 clf.fit(X_train, y_train)
